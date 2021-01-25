6768fcad99cfd7d732d1518d299ce62652c70edb
ardmos <46891528+ardmos@users.noreply.github.com>
2021-01-25 23:29:10 +0900

    SampleRoomScene 초기 세팅
    키 입력을 받아 캐릭터를 움직이는 작업 시작 준비.
    방을 조사.

--- Assets/Scripts/Utils/KeyInput_Controller.cs
+++ Assets/Scripts/Utils/KeyInput_Controller.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class KeyInput_Controller : MonoBehaviour
 {
+    //키인풋 컨트롤 만들기! 아무 오브젝트에나 이거 붙이면 방향키 입력 받아서 움직임! 
+
     // Start is called before the first frame update
     void Start()
     {
