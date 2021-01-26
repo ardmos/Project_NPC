70bcbbc43013cc5f21cd539bb1fa6f1b46c7d7d8
ardmos <46891528+ardmos@users.noreply.github.com>
2021-01-26 21:31:03 +0900

    스탸듀밸리움직임적용버젼
    타일맵도깔아봤음

--- Assets/Scripts/Utils/KeyInput_Controller.cs
+++ Assets/Scripts/Utils/KeyInput_Controller.cs
@@ -5,45 +5,39 @@ using UnityEngine;
 public class KeyInput_Controller : MonoBehaviour
 {
     //키인풋 컨트롤 만들기! 아무 오브젝트에나 이거 붙이면 방향키 입력 받아서 움직임! 
-    //타일맵의 셀 사이즈를 1로 했기 때문에,  이동도 1만큼 시키면 딱 딱 맞음.
+    //타일맵의 셀 사이즈를 1로 했기 때문에,  이동도 1만큼 시키면 딱 딱 맞음. <-- 바람의나라 방식
+    //스타듀밸리방식은 translate * 속도 이용
 
-    // Start is called before the first frame update
-    void Start()
-    {
-         
-    }
+    public float movespeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
-    {        
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            //print("LL");
-            gameObject.transform.Translate(Vector2.left);
-        }else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            //print("RR");
-            gameObject.transform.Translate(Vector2.right);
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            //print("UU");
-            gameObject.transform.Translate(Vector2.up);
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            //print("DD");
-            gameObject.transform.Translate(Vector2.down);
-        };
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+    }
 
 
     /// <summary>
     /// 아, 지금 이동을 쑥 쑥 해버려서, 충돌체크 하기도 전에 쑥 통과해버리는거지!!!  
     /// 이동방식 변경 ㄱㄱ !
     /// </summary>
- 
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         print(collision.gameObject.name);
