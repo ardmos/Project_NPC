139ef92f375e6841f3ec06c1cbbe11cd394903b8
ardmos <46891528+ardmos@users.noreply.github.com>
2021-02-02 18:18:45 +0900

    다이얼로그 4.0
    빨리넘기기 오류 현상 개선,
    페이드 인 아웃 프리팹 제작.(싱글턴)

---
+++ Assets/Scripts/SceneTransition/LevelChanger.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+
+    public Animator animator;
+
+    int levelToLoad;
+
+
+    #region 싱글턴
+    public static LevelChanger instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            FadeToNextLevel();
+        }
+    }
+
+
+    public void FadeToNextLevel()   //이걸 호출하면 빌드세팅 씬 넘버 순서상 다음 씬으로 넘어감.! 
+    {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()    //애니메이션이벤트를 사용했음.
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+}
+
